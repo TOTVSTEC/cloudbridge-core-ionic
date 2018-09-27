a02fbd801a79454d841167b0134bb14441e4362a
Rogério Ribeiro da Cruz <rogeriorc@gmail.com>
2018-09-27 15:56:33 -0300

    Suporte a cordova-android < 7.0.0

--- restore.js
+++ restore.js
@@ -20,10 +20,18 @@ class RestoreTask {
 	}
 
 	run() {
+		var pluginDir = path.join(__dirname, 'plugins', 'cordova-plugin-cdvwebsocketport'),
+			options = {
+				cwd: this.projectDir
+			};
+
 		shelljs.cp('-Rf', path.join(__dirname, 'src'), this.projectDir);
 		shelljs.cp('-Rf', path.join(__dirname, 'build'), this.projectDir);
-		shelljs.cp('-Rf', path.join(__dirname, 'plugins'), this.projectDir);
+		//shelljs.cp('-Rf', path.join(__dirname, 'plugins'), this.projectDir);
 
+		// Ionic nao respeita o "--no-fetch" e causa erro.
+		shelljs.exec('ionic cordova plugin add ' + pluginDir, options);
+		shelljs.exec('ionic cordova plugin save', options);
 
 		return Q();
 	}
